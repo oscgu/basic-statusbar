1f4be77adcbeb4d46d933681d4cf397f1b8838cf
Oscar Guertler <oscarguertler@gmail.com>
2022-01-09 00:05:58 +0100

    edit config

--- config.h
+++ config.h
@@ -7,30 +7,27 @@
  * Values between LowVal and HighVal get the MidIcon
  * Mode: 0
  *
+ * Example:
+  { ptm, {            33,           66,             "🧊",           "🌡️",           "🔥" },        0},
+ *
  * Static-Icon Status
  * .minArgs.icon = "your Icon or text"
  * Mode: 1
  *
+ * Example:
+  { mm, {   .minArgs.icon = "" },                                                                  1},
  */
-static Date datearr[10] = {
-        /*      Date    Icon */
-        { "24.12.2022", "🎅" }
-};
 
 static Module modules[] = {
 
-        /* Modules        LowVal        HighVal        LowIcon          MidIcon        HighIcon        Mode */
-        { ptm, {            33,           66,             "🧊",           "🌡️",           "🔥" },        0},
-        { plm, {            33,           66,             "🧊",           "🔥",           "🧯" },        0},
-
-        /* Modules      StaticIcon                                                                     Mode */
-        { mm, {   .minArgs.icon = "💾" },                                                                1},
-
-        { tm, {             10,            19,             "☕",           "⏳",           "🌙" },       0},
-        { dm, {   .minArgs.icon = "🗓️" },                                                                1},
+        /* Modules      StaticIcon     Mode */
+        { ptm, {  .minArgs.icon = "" }, 1},
+        { plm, {  .minArgs.icon = "" }, 1},
+        { mm, {   .minArgs.icon = "" }, 1},
+        { tm, {   .minArgs.icon = "" }, 1},
+        { dm, {   .minArgs.icon = "" }, 1},
 };
-
-/* .dates = datearr */
 
 /* char between modules */
 static char delimitter = ' ';
+
