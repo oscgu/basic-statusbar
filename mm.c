018f0b7880a377915c31a3c7ef4a7d0564a2a208
Oscar Guertler <oscarguertler@skiff.com>
2024-03-25 20:42:57 +0100

    style: fmt

--- mm.c
+++ mm.c
@@ -3,22 +3,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void mm(Args *arg, char *buff, int bufflen) {
-    char buffer[256];
-    int memTotal = 0, memAvailable = 0;
-    FILE *file = fopen("/proc/meminfo", "r");
-    if (file == NULL) return;
+void
+mm(Args *arg, char *buff, int bufflen)
+{
+        char buffer[256];
+        int memTotal = 0, memAvailable = 0;
+        FILE *file = fopen("/proc/meminfo", "r");
+        if (file == NULL) return;
 
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        if (strncmp(buffer, "MemTotal:", 9) == 0) {
-            sscanf(buffer + 9, "%d", &memTotal);
-        } else if (strncmp(buffer, "MemAvailable:", 13) == 0) {
-            sscanf(buffer + 13, "%d", &memAvailable);
+        while (fgets(buffer, sizeof(buffer), file) != NULL) {
+                if (strncmp(buffer, "MemTotal:", 9) == 0) {
+                        sscanf(buffer + 9, "%d", &memTotal);
+                } else if (strncmp(buffer, "MemAvailable:", 13) == 0) {
+                        sscanf(buffer + 13, "%d", &memAvailable);
+                }
         }
-    }
-    fclose(file);
+        fclose(file);
 
-    float usage = (float)(memTotal - memAvailable) / 1024.0 / 1024.0; // Convert to GB
+        float usage = (float) (memTotal - memAvailable) / 1024.0 /
+                      1024.0; // Convert to GB
 
-    snprintf(buff, bufflen, "%s%4.2fGb", getIcon(arg, usage), usage);
+        snprintf(buff, bufflen, "%s%4.2fGb", getIcon(arg, usage), usage);
 }
