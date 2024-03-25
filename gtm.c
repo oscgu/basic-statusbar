2f45342cff084f0b08e6cd77dccc4071d361178b
Oscar Guertler <oscarguertler@skiff.com>
2024-03-25 18:08:49 +0100

    fix: gpu temp module

--- gtm.c
+++ gtm.c
@@ -6,7 +6,7 @@ void
 gtm(Args *arg, char *buff, int bufflen)
 {
         char *fmt = "%s%d°C";
-        int temperature = read_temp("/sys/class/hwmon/hwmon1/temp1_input");
+        int temperature = read_temp("/sys/class/hwmon/hwmon0/temp1_input");
 
         snprintf(buff, bufflen, fmt, getIcon(arg, temperature), temperature);
 }
