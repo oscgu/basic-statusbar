55f585a40f65f76bc0ebd7eadf243865bec31c8d
Oscar Guertler <oscarguertler@skiff.com>
2024-03-25 18:09:19 +0100

    feat: add new script module

---
+++ script.c
@@ -0,0 +1,17 @@
+#include "util.h"
+#include <stdio.h>
+
+void
+script(Args *arg, char *buff, int bufflen)
+{
+    char *script = arg->icons[0];
+
+    FILE *f = popen(script, "r");
+    if (f == NULL) {
+        printf("failed to run script at: %s", script);
+        return;
+    }
+
+    fgets(buff, bufflen, f);
+    pclose(f);
+}
---
+++ script.h
@@ -0,0 +1,5 @@
+#ifndef SCRIPT_H
+#define SCRIPT_H
+#include "arg.h"
+void script(Args *, char *, int);
+#endif // SCRIPT_H
