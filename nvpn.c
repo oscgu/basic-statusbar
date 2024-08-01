76a949b3bbfdd4279c3ec8343fb450e9a3364ff1
Oscar Guertler <oscarguertler@skiff.com>
2024-08-01 15:34:44 +0200

    feat: additionally show country when using nvpn

--- nvpn.c
+++ nvpn.c
@@ -6,22 +6,26 @@
 void
 nvpn(Args *arg, char *buff, int bufflen)
 {
-        char textbuff[1035];
-        char vpnstatus[20];
+        char textbuff[19];
+        char country[20] = {0};
+        char vpnstatus[40];
 
         FILE *fp = popen("/bin/nordvpn status", "r");
         if (!fp) return;
 
-        while (fscanf(fp, " %1023s", textbuff) == 1) {
+        while (fscanf(fp, " %18s", textbuff) == 1) {
                 if (strcmp(textbuff, "Status:") == 0) {
                         fscanf(fp, " %s", vpnstatus);
+                }
+                if (strcmp(textbuff, "Country:") == 0) {
+                        fscanf(fp, " %s", country);
                 }
         }
         pclose(fp);
 
         if (strcmp(vpnstatus, "Connected") == 0) {
-                snprintf(buff, bufflen, "%s", getIcon(arg, 0));
+                snprintf(buff, bufflen, "%s %s", getIcon(arg, 0), country);
         } else {
-                snprintf(buff, bufflen, "%s", getIcon(arg, 1));
+                snprintf(buff, bufflen, "%s %s", getIcon(arg, 1), country);
         }
 }
